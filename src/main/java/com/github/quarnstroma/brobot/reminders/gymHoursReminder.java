408ed971d4d0f8d052d157ed0b3074e38f8ddc59
Anthony Quarnstrom <quarnstroma@gmail.com>
2016-09-11 20:33:25 -0600

    add exception parsing page

--- src/main/java/com/github/quarnstroma/brobot/reminders/gymHoursReminder.java
+++ src/main/java/com/github/quarnstroma/brobot/reminders/gymHoursReminder.java
@@ -23,7 +23,11 @@ public class gymHoursReminder extends TimerTask{
         String message = null;
         try {
             doc = Jsoup.connect("https://www.facebook.com/BoiseStateRecreation").get();
+            System.out.println(doc);
             Element state = doc.getElementsByClass("_1xm9").first();
+            if(null==state){
+                state = doc.getElementsByClass("_1xma").first();
+            }
             System.out.println(state.text());
             Element time = doc.select("span._c24._50f3").first();
             System.out.println(time.text());
@@ -31,8 +35,10 @@ public class gymHoursReminder extends TimerTask{
         } catch (IOException e) {
             message = "I failed to get the time. Please check my logs.";
             System.out.println(e);
+        } catch (NullPointerException e){
+            message = "I'm broken.";
         }
 
-        //brobot.sendSMS(message);
+        brobot.sendSMS(message);
     }
 }
